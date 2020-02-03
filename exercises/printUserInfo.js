287e12841d56d58dd2421ca0cbb3c9f7e9f283d3
Keith Ng <keith.jk.ng@gmail.com>
2020-02-03 14:38:50 -0800

    Doctor Strange is strange!

--- exercises/printUserInfo.js
+++ exercises/printUserInfo.js
@@ -7,11 +7,13 @@ let process = require('process');
 
 let user = {};
 
-user['firstName'] = '<Change This>';
-user['lastName'] = '<Change This>';
-user['currentLocation'] = '<Change This>';
-user['favoriteSongs'] = ['One Song', 'Another Great Song', 'Last Great Song'];
-user['age'] = 108;
+user['firstName'] = 'Stephen';
+user['lastName'] = 'Strange';
+user['currentLocation'] = 'New York';
+user['favoriteSongs'] = ['Smote and Mirrors', 'The Eyes Have It', 'Hippocratic Hypocrite'];
+user['age'] = 40;
+user['favoriteFood'] = ['Onigiri'];
+user['favoriteMovies'] = ['Doctor Strange', 'Guardians of the Galaxy Vol. 2', 'Spider-Man: Homecoming', 'Thor: Ragnarok', 'Black Panther'];
 
 // To get started, set user['canBeDisplayed'] to true instead of false
 user['canBeDisplayed'] = true;
@@ -27,16 +29,55 @@ console.log();
 console.log(`How's the weather in ${user['currentLocation']}?`);
 console.log();
 
-if (user['age'] >= 100) {
-  console.log(`Wow! You're ${user['age']} years old? Fewer than 0.2% reach that age.`);
+if (user['age'] >= 40) {
+  console.log(`Wow! You're ${user['age']} years old? Time for a crazy mid-life crisis!`);
   console.log();
 }
 
 console.log('Looks like you have great taste in music. Your favorite songs:');
 
-for (let i = 0; i < user['favoriteSongs'].length; i += 1) {
+printFavorites(user['favoriteSongs'])
+
+/** Given code for favortie songs list (for reference)
+  for (let i = 0; i < user['favoriteSongs'].length; i += 1) {
   let songPosition = i + 1;
   let songName = user['favoriteSongs'][i];
 
   console.log(`${songPosition}. ${songName}`);
+}
+*/
+
+console.log('I see that you eat ' + user['favoriteFood'] + '.')
+
+if ((user['favoriteFood'].toString()) === 'Hot Dogs') {
+  console.log(user['favoriteFood'] + ' are really processed!');
+} else if ((user['favoriteFood'].toString()) === 'Onigiri') {
+  console.log(user['favoriteFood'] + ' sounds delicious!');
+} else {
+  console.log(user['favoriteFood'] + ' is a good choice')
+}
+
+console.log()
+console.log('Your favorite movies are:')
+
+printFavorites(user['favoriteMovies'])
+
+/** Initial code for movie list (for reference)
+  for (let i = 0; i < user['favoriteMovies'].length; i += 1) {
+  let movieNumber = i + 1;
+  let movie = user['favoriteMovies'][i];
+
+  console.log(`${movieNumber}. ${movie}`);
+}
+console.log()
+*/
+
+function printFavorites (inputArray) {
+  for (let i = 0; i < inputArray.length; i += 1) {
+    let movieNumber = i + 1;
+    let movie = inputArray[i];
+
+    console.log(`${movieNumber}. ${movie}`);
+  }
+  console.log()
 }
